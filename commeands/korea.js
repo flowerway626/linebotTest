5023ee03e42916bd37bc6d16fbf60efb73c7fb15
PCdoremiso <flowerway626@gmail.com>
2022-11-30 21:10:59 +0800

    test2

--- commeands/korea.js
+++ commeands/korea.js
@@ -3,7 +3,7 @@ import * as cheerio from 'cheerio'
 // import writejson from '../utils/writejson.js'
 // import flex from '../flexs/flex.js'
 
-export default async () => {
+export default async (event) => {
   // includes()
   try {
     // const country = event.message.text.substr(0, 2)
@@ -14,35 +14,35 @@ export default async () => {
     // encodeURI
     const { data } = await axios.get(url)
     const $ = cheerio.load(data, { decodeEntities: false })
-    console.log($.html())
+    event.reply($.html())
     const dramas = []
-    if ($('.box_inner').find('ul').text() !== '') {
-      $('.category-list li').each(function (i) {
-        // const replyFlex = JSON.parse(JSON.stringify(flex))
-        // replyFlex.body.contents[0].url = $(this).find('.movie_foto img').attr('src')
-        // dramas.push($(this).find('.movie_foto img').attr('src'))
-        // replyFlex.body.contents[0].action.text = $(this).find('.movielist_info h2').text().trim()
-        // replyFlex.body.contents[2].contents[0].contents[0].contents[0].text = $(this).find('.movielist_info h2').text().trim()
-        // replyFlex.body.contents[2].contents[0].contents[1].contents[0].contents[0].text = $(this).find('.movielist_info .season').text().trim()
-        // dramas.push(replyFlex)
-      })
-      console.log(dramas)
-      // const reply = {
-      //   type: 'flex',
-      //   altText: '排行查詢結果',
-      //   contents: {
-      //     type: 'carousel',
-      //     contents: dramas
-      //   }
-      // }
+    // if ($('.box_inner').find('ul').text() !== '') {
+    //   $('.category-list li').each(function (i) {
+    // const replyFlex = JSON.parse(JSON.stringify(flex))
+    // replyFlex.body.contents[0].url = $(this).find('.movie_foto img').attr('src')
+    // dramas.push($(this).find('.movie_foto img').attr('src'))
+    // replyFlex.body.contents[0].action.text = $(this).find('.movielist_info h2').text().trim()
+    // replyFlex.body.contents[2].contents[0].contents[0].contents[0].text = $(this).find('.movielist_info h2').text().trim()
+    // replyFlex.body.contents[2].contents[0].contents[1].contents[0].contents[0].text = $(this).find('.movielist_info .season').text().trim()
+    // dramas.push(replyFlex)
+    // })
+    console.log(dramas)
+    // const reply = {
+    //   type: 'flex',
+    //   altText: '排行查詢結果',
+    //   contents: {
+    //     type: 'carousel',
+    //     contents: dramas
+    //   }
+    // }
 
-      // event.reply(reply)
-      // writejson(reply, 'dramas')
-    } else console.log('失敗')
+    // event.reply(reply)
+    // writejson(reply, 'dramas')
+    // } else console.log('失敗')
     // event.reply('查無資料，請更換檢索條件')
     // return dramaNums
   } catch (error) {
-    // event.reply('發生錯誤，請稍後再試11')
+    event.reply('發生錯誤，請稍後再試11')
     console.error(error)
   }
 }
